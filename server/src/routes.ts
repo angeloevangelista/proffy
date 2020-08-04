1f548216ba7a02bbb1332e41aec565dadef19c3a
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-04 20:07:33 -0300

    Listando Connections

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import ClassesController from './app/controllers/ClassesController';
+import ConnectionsController from './app/controllers/ConnectionsController';
 
 const routes = Router();
 
 routes.get('/classes', ClassesController.index);
 routes.post('/classes', ClassesController.create);
+
+routes.get('/connections', ConnectionsController.index);
+routes.post('/connections', ConnectionsController.create);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -7,6 +8,8 @@ const port = 3333;
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.listen(port, () => {
   console.clear();
