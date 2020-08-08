8490a3cefbbf157aabd629341579e1213792d63c
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-08-07 23:32:18 -0300

    Landing page mobile

---
+++ mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,70 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8257e5',
+    justifyContent: 'center',
+    padding: 40,
+  },
+
+  banner: {
+    width: '100%',
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+
+  title: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#fff',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+
+  titleBold: {
+    fontFamily: 'Poppins_600SemiBold',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+
+  button: {
+    height: 150,
+    width: '45%',
+    borderRadius: 8,
+    backgroundColor: '#444',
+    padding: 24,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  buttonPrimary: {
+    backgroundColor: '#9871f5',
+  },
+
+  buttonSecondary: {
+    backgroundColor: '#04d361',
+  },
+
+  buttonText: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#fff',
+    fontSize: 20,
+  },
+
+  totalConnections: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#d4c2ff',
+    fontSize: 12,
+    lineHeight: 20,
+    maxHeight: 140,
+    marginTop: 40,
+  },
+});
+
+export default styles;
